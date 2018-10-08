7687c2a9f09517e9470e8495d1df0e16a800d28f
sep <itsasimulation@protonmail.com>
2018-10-08 06:53:26 +0000

    enter the commit message for your changes. Lines starting 	modified:   server.py 	modified:   templates/main.html Add in-memory jpeg compression. No more saving files and reloading. Display visitor IP address

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from flask import Flask
 from flask import render_template
 from flask import Response
+from flask import request
 from cv2 import *
 
 app = Flask(__name__)
@@ -10,18 +11,17 @@ app = Flask(__name__)
 @app.route("/")
 def main():
     time_str = str(datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-    return render_template('main.html', time=time_str)
+    return render_template('main.html', time=time_str, visitor_ip=str(request.remote_addr))
 
 
 def gen():
     cam = VideoCapture(0)
+    encode_param = [int(IMWRITE_JPEG_QUALITY), 90]
     while True:
         cam_opened, img = cam.read()
 
-        if cam_opened:
-            imwrite("a.jpg", img)
-            # cam.release()
-        frame = open('a.jpg', 'rb').read()
+        ret, jpg_frame = imencode('.jpg', img, encode_param)
+        frame = jpg_frame.tobytes() # jpg_frame is of type numpy.ndarray
 
         yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
